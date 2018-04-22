533c4883d575bf769811b2d5e55e6524e8ab1287
dkwazhere <dhealth77@gmail.com>
2018-04-22 16:27:59 -0700

    added jumbotron

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => (
+  <div>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">New York Times Scraper</h1>
+        <p class="lead">Search for an article, Save an article, Comment on the article!</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Navbar;
